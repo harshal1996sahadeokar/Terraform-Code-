45063d4ff05e9c601fa12f0ae0457a9c5b1397e0
HARSHAL SAHADEOKAR <131584127+harshal1996sahadeokar@users.noreply.github.com>
2024-12-14 13:45:48 +0530

    Create Variables.tf

---
+++ Variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  default = "tomcat-key"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+#variable "public_key_path" {
+  #default = "~/.ssh/id_rsa.pub"
+#}
